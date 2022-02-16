3fd1ebf13121e0d0ccf817c39baa0ddbf224e3da
raymondcase <99825946+raymondcase@users.noreply.github.com>
2022-02-16 22:27:56 +0000

    feat: Add forge

---
+++ src/utils/YAMLogic3.sol
@@ -0,0 +1,643 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.10;
+
+// Interfaces
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+// Libraries
+import {SafeERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+
+// Contracts
+import {YAMGovernanceToken} from "./YAMGovernanceToken.sol";
+
+
+contract YAMToken is YAMGovernanceToken {
+    // Modifiers
+    modifier onlyGov() {
+        require(msg.sender == gov);
+        _;
+    }
+
+    modifier onlyRebaser() {
+        require(msg.sender == rebaser);
+        _;
+    }
+
+    modifier onlyMinter() {
+        require(
+            msg.sender == rebaser
+            || msg.sender == gov
+            || msg.sender == incentivizer
+            || msg.sender == migrator,
+            "not minter"
+        );
+        _;
+    }
+
+    modifier validRecipient(address to) {
+        require(to != address(0x0));
+        require(to != address(this));
+        _;
+    }
+
+    function initialize(
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_
+    )
+        public
+    {
+        require(yamsScalingFactor == 0, "already initialized");
+        name = name_;
+        symbol = symbol_;
+        decimals = decimals_;
+    }
+
+
+    /**
+    * @notice Computes the current max scaling factor
+    */
+    function maxScalingFactor()
+        override
+        external
+        view
+        returns (uint256)
+    {
+        return _maxScalingFactor();
+    }
+
+    function _maxScalingFactor()
+        internal
+        view
+        returns (uint256)
+    {
+        // scaling factor can only go up to 2**256-1 = initSupply * yamsScalingFactor
+        // this is used to check if yamsScalingFactor will be too high to compute balances when rebasing.
+        return type(uint256).max / initSupply;
+    }
+
+    /**
+    * @notice Mints new tokens, increasing totalSupply, initSupply, and a users balance.
+    * @dev Limited to onlyMinter modifier
+    */
+    function mint(address to, uint256 amount)
+        override
+        external
+        onlyMinter
+        returns (bool)
+    {
+        _mint(to, amount);
+        return true;
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+    {
+      if (msg.sender == migrator) {
+        // migrator directly uses v2 balance for the amount
+
+        // increase initSupply
+        initSupply = initSupply + amount;
+
+        // get external value
+        uint256 scaledAmount = _yamToFragment(amount);
+
+        // increase totalSupply
+        totalSupply = totalSupply + scaledAmount;
+
+        // make sure the mint didnt push maxScalingFactor too low
+        require(yamsScalingFactor <= _maxScalingFactor(), "max scaling factor too low");
+
+        // add balance
+        _yamBalances[to] = _yamBalances[to] + amount;
+
+        // add delegates to the minter
+        _moveDelegates(address(0), _delegates[to], amount);
+        emit Mint(to, scaledAmount);
+        emit Transfer(address(0), to, scaledAmount);
+      } else {
+        // increase totalSupply
+        totalSupply = totalSupply + amount;
+
+        // get underlying value
+        uint256 yamValue = _fragmentToYam(amount);
+
+        // increase initSupply
+        initSupply = initSupply + yamValue;
+
+        // make sure the mint didnt push maxScalingFactor too low
+        require(yamsScalingFactor <= _maxScalingFactor(), "max scaling factor too low");
+
+        // add balance
+        _yamBalances[to] = _yamBalances[to] + yamValue;
+
+        // add delegates to the minter
+        _moveDelegates(address(0), _delegates[to], yamValue);
+        emit Mint(to, amount);
+        emit Transfer(address(0), to, amount);
+      }
+    }
+
+    /**
+    * @notice Burns tokens from msg.sender, decreases totalSupply, initSupply, and a users balance.
+    */
+
+    function burn(uint256 amount)
+        override
+        external
+        returns (bool)
+    {
+        _burn(amount);
+        return true;
+    }
+
+    function _burn(uint256 amount)
+        internal
+    {
+                // decrease totalSupply
+        totalSupply = totalSupply - amount;
+
+        // get underlying value
+        uint256 yamValue = _fragmentToYam(amount);
+
+        // decrease initSupply
+        initSupply = initSupply - yamValue;
+
+        // decrease balance
+        _yamBalances[msg.sender] = _yamBalances[msg.sender] - yamValue;
+
+        // add delegates to the minter
+        _moveDelegates(_delegates[msg.sender], address(0), yamValue);
+        emit Burn(msg.sender, amount);
+        emit Transfer(msg.sender, address(0), amount);
+        }
+
+    /**
+    * @notice Mints new tokens using underlying amount, increasing totalSupply, initSupply, and a users balance.
+    * @dev Limited to onlyMinter modifier
+    */
+    function mintUnderlying(address to, uint256 amount)
+        external
+        onlyMinter
+        returns (bool)
+    {
+        _mintUnderlying(to, amount);
+        return true;
+    }
+
+    function _mintUnderlying(address to, uint256 amount)
+        internal
+    {
+
+        // increase initSupply
+        initSupply = initSupply + amount;
+
+        // get external value
+        uint256 scaledAmount = _yamToFragment(amount);
+
+        // increase totalSupply
+        totalSupply = totalSupply + scaledAmount;
+
+        // make sure the mint didnt push maxScalingFactor too low
+        require(yamsScalingFactor <= _maxScalingFactor(), "max scaling factor too low");
+
+        // add balance
+        _yamBalances[to] = _yamBalances[to] + amount;
+
+        // add delegates to the minter
+        _moveDelegates(address(0), _delegates[to], amount);
+        emit Mint(to, scaledAmount);
+        emit Transfer(address(0), to, scaledAmount);
+   
+    }
+
+    /**
+     * @dev Transfer underlying balance to a specified address.
+     * @param to The address to transfer to.
+     * @param value The amount to be transferred.
+     * @return True on success, false otherwise.
+     */
+    function transferUnderlying(address to, uint256 value)
+        external
+        validRecipient(to)
+        returns (bool)
+    {
+        // sub from balance of sender
+        _yamBalances[msg.sender] = _yamBalances[msg.sender] - value;
+
+        // add to balance of receiver
+        _yamBalances[to] = _yamBalances[to] + value;
+        emit Transfer(msg.sender, to, _yamToFragment(value));
+
+        _moveDelegates(_delegates[msg.sender], _delegates[to], value);
+        return true;
+    }
+    
+    /* - ERC20 functionality - */
+
+    /**
+    * @dev Transfer tokens to a specified address.
+    * @param to The address to transfer to.
+    * @param value The amount to be transferred.
+    * @return True on success, false otherwise.
+    */
+    function transfer(address to, uint256 value)
+        override
+        external
+        validRecipient(to)
+        returns (bool)
+    {
+        // underlying balance is stored in yams, so divide by current scaling factor
+
+        // note, this means as scaling factor grows, dust will be untransferrable.
+        // minimum transfer value == yamsScalingFactor / 1e24;
+
+        // get amount in underlying
+        uint256 yamValue = _fragmentToYam(value);
+
+        // sub from balance of sender
+        _yamBalances[msg.sender] = _yamBalances[msg.sender] - yamValue;
+
+        // add to balance of receiver
+        _yamBalances[to] = _yamBalances[to] + yamValue;
+        emit Transfer(msg.sender, to, value);
+
+        _moveDelegates(_delegates[msg.sender], _delegates[to], yamValue);
+        return true;
+    }
+
+    /**
+    * @dev Transfer tokens from one address to another.
+    * @param from The address you want to send tokens from.
+    * @param to The address you want to transfer to.
+    * @param value The amount of tokens to be transferred.
+    */
+    function transferFrom(address from, address to, uint256 value)
+        override
+        external
+        validRecipient(to)
+        returns (bool)
+    {
+        // decrease allowance
+        _allowedFragments[from][msg.sender] = _allowedFragments[from][msg.sender] - value;
+
+        // get value in yams
+        uint256 yamValue = _fragmentToYam(value);
+
+        // sub from from
+        _yamBalances[from] = _yamBalances[from] - yamValue;
+        _yamBalances[to] = _yamBalances[to] + yamValue;
+        emit Transfer(from, to, value);
+
+        _moveDelegates(_delegates[from], _delegates[to], yamValue);
+        return true;
+    }
+
+    /**
+    * @param who The address to query.
+    * @return The balance of the specified address.
+    */
+    function balanceOf(address who)
+      override
+      external
+      view
+      returns (uint256)
+    {
+      return _yamToFragment(_yamBalances[who]);
+    }
+
+    /** @notice Currently returns the internal storage amount
+    * @param who The address to query.
+    * @return The underlying balance of the specified address.
+    */
+    function balanceOfUnderlying(address who)
+      override
+      external
+      view
+      returns (uint256)
+    {
+      return _yamBalances[who];
+    }
+
+    /**
+     * @dev Function to check the amount of tokens that an owner has allowed to a spender.
+     * @param owner_ The address which owns the funds.
+     * @param spender The address which will spend the funds.
+     * @return The number of tokens still available for the spender.
+     */
+    function allowance(address owner_, address spender)
+        override
+        external
+        view
+        returns (uint256)
+    {
+        return _allowedFragments[owner_][spender];
+    }
+
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of
+     * msg.sender. This method is included for ERC20 compatibility.
+     * increaseAllowance and decreaseAllowance should be used instead.
+     * Changing an allowance with this method brings the risk that someone may transfer both
+     * the old and the new allowance - if they are both greater than zero - if a transfer
+     * transaction is mined before the later approve() call is mined.
+     *
+     * @param spender The address which will spend the funds.
+     * @param value The amount of tokens to be spent.
+     */
+    function approve(address spender, uint256 value)
+        override
+        external
+        returns (bool)
+    {
+        _allowedFragments[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    /**
+     * @dev Increase the amount of tokens that an owner has allowed to a spender.
+     * This method should be used instead of approve() to avoid the double approval vulnerability
+     * described above.
+     * @param spender The address which will spend the funds.
+     * @param addedValue The amount of tokens to increase the allowance by.
+     */
+    function increaseAllowance(address spender, uint256 addedValue)
+        override
+        external
+        returns (bool)
+    {
+        _allowedFragments[msg.sender][spender] =
+            _allowedFragments[msg.sender][spender] + addedValue;
+        emit Approval(msg.sender, spender, _allowedFragments[msg.sender][spender]);
+        return true;
+    }
+
+    /**
+     * @dev Decrease the amount of tokens that an owner has allowed to a spender.
+     *
+     * @param spender The address which will spend the funds.
+     * @param subtractedValue The amount of tokens to decrease the allowance by.
+     */
+    function decreaseAllowance(address spender, uint256 subtractedValue)
+        override
+        external
+        returns (bool)
+    {
+        uint256 oldValue = _allowedFragments[msg.sender][spender];
+        if (subtractedValue >= oldValue) {
+            _allowedFragments[msg.sender][spender] = 0;
+        } else {
+            _allowedFragments[msg.sender][spender] = oldValue - subtractedValue;
+        }
+        emit Approval(msg.sender, spender, _allowedFragments[msg.sender][spender]);
+        return true;
+    }
+
+    // --- Approve by signature ---
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    )
+        external
+    {
+        require(block.timestamp <= deadline, "YAM/permit-expired");
+
+        bytes32 digest =
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    DOMAIN_SEPARATOR,
+                    keccak256(
+                        abi.encode(
+                            PERMIT_TYPEHASH,
+                            owner,
+                            spender,
+                            value,
+                            nonces[owner]++,
+                            deadline
+                        )
+                    )
+                )
+            );
+
+        require(owner != address(0), "YAM/invalid-address-0");
+        require(owner == ecrecover(digest, v, r, s), "YAM/invalid-permit");
+        _allowedFragments[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
+    /* - Governance Functions - */
+
+    /** @notice sets the rebaser
+     * @param rebaser_ The address of the rebaser contract to use for authentication.
+     */
+    function _setRebaser(address rebaser_)
+        override
+        external
+        onlyGov
+    {
+        address oldRebaser = rebaser;
+        rebaser = rebaser_;
+        emit NewRebaser(oldRebaser, rebaser_);
+    }
+
+    /** @notice sets the migrator
+     * @param migrator_ The address of the migrator contract to use for authentication.
+     */
+    function _setMigrator(address migrator_)
+        external
+        onlyGov
+    {
+        address oldMigrator = migrator_;
+        migrator = migrator_;
+        emit NewMigrator(oldMigrator, migrator_);
+    }
+
+    /** @notice sets the incentivizer
+     * @param incentivizer_ The address of the rebaser contract to use for authentication.
+     */
+    function _setIncentivizer(address incentivizer_)
+        override
+        external
+        onlyGov
+    {
+        address oldIncentivizer = incentivizer;
+        incentivizer = incentivizer_;
+        emit NewIncentivizer(oldIncentivizer, incentivizer_);
+    }
+
+    /** @notice sets the pendingGov
+     * @param pendingGov_ The address of the rebaser contract to use for authentication.
+     */
+    function _setPendingGov(address pendingGov_)
+        override
+        external
+        onlyGov
+    {
+        address oldPendingGov = pendingGov;
+        pendingGov = pendingGov_;
+        emit NewPendingGov(oldPendingGov, pendingGov_);
+    }
+
+    /** @notice allows governance to assign delegate to self
+     *
+     */
+    function _acceptGov()
+        override
+        external
+    {
+        require(msg.sender == pendingGov, "!pending");
+        address oldGov = gov;
+        gov = pendingGov;
+        pendingGov = address(0);
+        emit NewGov(oldGov, gov);
+    }
+
+    function assignSelfDelegate(address nonvotingContract)
+        external
+        onlyGov
+    {
+        address delegate = _delegates[nonvotingContract];
+        require( delegate == address(0), "!address(0)" );
+        // assigns delegate to self only
+        _delegate(nonvotingContract, nonvotingContract);
+    }
+
+    /* - Extras - */
+
+    /**
+    * @notice Initiates a new rebase operation, provided the minimum time period has elapsed.
+    *
+    * @dev The supply adjustment equals (totalSupply * DeviationFromTargetRate) / rebaseLag
+    *      Where DeviationFromTargetRate is (MarketOracleRate - targetRate) / targetRate
+    *      and targetRate is CpiOracleRate / baseCpi
+    */
+    function rebase(
+        uint256 epoch,
+        uint256 indexDelta,
+        bool positive
+    )
+        override
+        external
+        onlyRebaser
+        returns (uint256)
+    {
+        // no change
+        if (indexDelta == 0) {
+          emit Rebase(epoch, yamsScalingFactor, yamsScalingFactor);
+          return totalSupply;
+        }
+
+        // for events
+        uint256 prevYamsScalingFactor = yamsScalingFactor;
+
+
+        if (!positive) {
+            // negative rebase, decrease scaling factor
+            yamsScalingFactor = (yamsScalingFactor * (BASE - indexDelta)) / BASE;
+        } else {
+            // positive reabse, increase scaling factor
+            uint256 newScalingFactor = (yamsScalingFactor * (BASE + indexDelta)) / BASE;
+            if (newScalingFactor < _maxScalingFactor()) {
+                yamsScalingFactor = newScalingFactor;
+            } else {
+                yamsScalingFactor = _maxScalingFactor();
+            }
+        }
+
+        // update total supply, correctly
+        totalSupply = _yamToFragment(initSupply);
+
+        emit Rebase(epoch, prevYamsScalingFactor, yamsScalingFactor);
+        return totalSupply;
+    }
+
+    function yamToFragment(uint256 yam)
+        override
+        external
+        view
+        returns (uint256)
+    {
+        return _yamToFragment(yam);
+    }
+
+    function fragmentToYam(uint256 value)
+        override
+        external
+        view
+        returns (uint256)
+    {
+        return _fragmentToYam(value);
+    }
+
+    function _yamToFragment(uint256 yam)
+        internal
+        view
+        returns (uint256)
+    {
+        return (yam * yamsScalingFactor) / internalDecimals;
+    }
+
+    function _fragmentToYam(uint256 value)
+        internal
+        view
+        returns (uint256)
+    {
+        return (value * internalDecimals) / yamsScalingFactor;
+    }
+
+    // Rescue tokens
+    function rescueTokens(
+        address token,
+        address to,
+        uint256 amount
+    )
+        external
+        onlyGov
+        returns (bool)
+    {
+        // transfer to
+        SafeERC20.safeTransfer(IERC20(token), to, amount);
+        return true;
+    }
+}
+
+contract YAMLogic3 is YAMToken {
+    /**
+     * @notice Initialize the new money market
+     * @param name_ ERC-20 name of this token
+     * @param symbol_ ERC-20 symbol of this token
+     * @param decimals_ ERC-20 decimal precision of this token
+     */
+    function initialize(
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_,
+        address initial_owner,
+        uint256 initTotalSupply_
+    )
+        public
+    {
+        super.initialize(name_, symbol_, decimals_);
+
+        yamsScalingFactor = BASE;
+        initSupply = _fragmentToYam(initTotalSupply_);
+        totalSupply = initTotalSupply_;
+        _yamBalances[initial_owner] = initSupply;
+
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                DOMAIN_TYPEHASH,
+                keccak256(bytes(name)),
+                getChainId(),
+                address(this)
+            )
+        );
+    }
+}
