2fd57e665769a96224b93b4621397a5350ffde64
ethedev <2432272+ethedev@users.noreply.github.com>
2022-05-03 21:04:22 +0400

    update proposals april

---
+++ contracts/2022/04/Proposal25.sol
@@ -0,0 +1,156 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.10;
+
+import {VestingPool} from "../../../utils/VestingPool.sol";
+import {YAMTokenInterface} from "../../../utils/YAMTokenInterface.sol";
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+interface IYVault {
+    function deposit(uint256 amount, address recipient)
+        external
+        returns (uint256);
+
+    function withdraw(uint256 amount) external returns (uint256);
+}
+
+contract Proposal25 {
+    /// @dev Contracts and ERC20 addresses
+    IERC20 internal constant WETH =
+        IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IERC20 internal constant YAM =
+        IERC20(0x0AaCfbeC6a24756c20D41914F2caba817C0d8521);
+    IERC20 internal constant UMA =
+        IERC20(0x04Fa0d235C4abf4BcF4787aF4CF447DE572eF828);
+    IYVault internal constant yUSDCV2 =
+        IYVault(0x5f18C75AbDAe578b483E5F43f12a39cF75b973a9);
+    IYVault internal constant yUSDC =
+        IYVault(0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE);
+    IERC20 internal constant USDC =
+        IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    IERC20 internal constant SUSHI =
+        IERC20(0x6B3595068778DD592e39A122f4f5a5cF09C90fE2);
+    VestingPool internal constant pool =
+        VestingPool(0xDCf613db29E4d0B35e7e15e93BF6cc6315eB0b82);
+    address internal constant RESERVES =
+        0x97990B693835da58A281636296D2Bf02787DEa17;
+    address internal constant MULTISIG =
+        0x744D16d200175d20E6D8e5f405AEfB4EB7A962d1;
+    IERC20 internal constant SCJAN6 =
+        IERC20(0x0B4470515228625ef51E6DB32699989046fCE4De);
+    IERC20 internal constant SCDEC2 =
+        IERC20(0xf447EF251De50E449107C8D687d10C91e0b7e4D4);
+    IERC20 internal constant SCNOV3 =
+        IERC20(0xff8f62855fD433347BbE62f1292F905f7aC1DF9d);
+    IUSynthFarming internal UGASFarming =
+        IUSynthFarming(0xDFE435ade40Bbf7476a0847d5ae6B1df9BD5Ba30);
+    IUSynthFarming internal USTONKSFarming =
+        IUSynthFarming(0x9789204c43bbc03E9176F2114805B68D0320B31d);
+    IUSynthFarming internal UPUNKSFarming =
+        IUSynthFarming(0xA5163cE5331413715c1023f28499cbdC9a8b8b3E);
+
+    function execute() public {
+        // Stablecoin transfers
+
+        // Withdraw USDC
+        IERC20(address(USDC)).transferFrom(
+            RESERVES,
+            address(this),
+            IERC20(address(USDC)).balanceOf(RESERVES)
+        );
+
+        // // E
+        // USDC.transfer(
+        //     0x8A8acf1cEcC4ed6Fe9c408449164CE2034AdC03f,
+        //     yearlyToMonthlyUSD(119004, 1)
+        // );
+        // // Chilly
+        // USDC.transfer(
+        //     0x01e0C7b70E0E05a06c7cC8deeb97Fa03d6a77c9C,
+        //     yearlyToMonthlyUSD(93800, 1)
+        // );
+        // // Designer
+        // USDC.transfer(
+        //     0x3FdcED6B5C1f176b543E5E0b841cB7224596C33C,
+        //     yearlyToMonthlyUSD(92400, 1)
+        // );
+        // // Ross
+        // USDC.transfer(
+        //     0x88c868B1024ECAefDc648eb152e91C57DeA984d0,
+        //     yearlyToMonthlyUSD(84000, 1)
+        // );
+
+        uint256 usdcBalance = USDC.balanceOf(address(this));
+        USDC.transfer(RESERVES, usdcBalance);
+
+        // Synths
+
+        // Exit position for UGAS USTONKS UPUNKS
+        UGASFarming._approveExit();
+        USTONKSFarming._approveExit();
+        UPUNKSFarming._approveExit();
+
+        // Claim reward tokens to treasury
+        UGASFarming._getTokenFromHere(address(UMA));
+        USTONKSFarming._getTokenFromHere(address(UMA));
+        UPUNKSFarming._getTokenFromHere(address(UMA));
+        UGASFarming._getTokenFromHere(address(SCJAN6));
+        UPUNKSFarming._getTokenFromHere(address(SCJAN6));
+        UGASFarming._getTokenFromHere(address(SCDEC2));
+        UPUNKSFarming._getTokenFromHere(address(SCDEC2));
+        UGASFarming._getTokenFromHere(address(SCNOV3));
+        UPUNKSFarming._getTokenFromHere(address(SCNOV3));
+
+        // Yam transfers
+
+        // // E
+        // YAM.transfer(
+        //     0x8A8acf1cEcC4ed6Fe9c408449164CE2034AdC03f,
+        //     18478 * (10**18)
+        // );
+        // // Chilly
+        // YAM.transfer(
+        //     0x01e0C7b70E0E05a06c7cC8deeb97Fa03d6a77c9C,
+        //     14565 * (10**18)
+        // );
+        // // Designer
+        // YAM.transfer(
+        //     0x3FdcED6B5C1f176b543E5E0b841cB7224596C33C,
+        //     14348 * (10**18)
+        // );
+        // // Ross
+        // YAM.transfer(
+        //     0x88c868B1024ECAefDc648eb152e91C57DeA984d0,
+        //     10870 * (10**18)
+        // );
+        // // Mona
+        // YAM.transfer(
+        //     0xdADc6F71986643d9e9CB368f08Eb6F1333F6d8f9,
+        //     7243 * (10**18)
+        // );
+
+        // Transfer vesting pool YAM to reserves
+        pool.payout(pool.openStream(RESERVES, 0, 2086282107599161416658680858));
+
+        // Transfer 25k YAM to the multisig
+        YAM.transfer(MULTISIG, 25000 * (10**18));
+        uint256 yamBalance = YAM.balanceOf(address(this));
+        YAM.transfer(RESERVES, yamBalance);
+    }
+
+    function yearlyToMonthlyUSD(uint256 yearlyUSD, uint256 months)
+        internal
+        pure
+        returns (uint256)
+    {
+        return (((yearlyUSD * (10**6)) / uint256(12)) * months);
+    }
+}
+
+interface IUSynthFarming {
+    function _approveEnter() external;
+    function _approveExit() external;
+    function _getTokenFromHere(address token) external;
+    function update_twap() external;
+    function enter() external;
+    function exit() external;
+}
---
+++ contracts/2022/04/Proposal25.t.sol
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.10;
+
+import "../../../utils/YAMTest.sol";
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {YAMDelegate3} from "../../../utils/YAMDelegate3.sol";
+import {YAMTokenInterface} from "../../../utils/YAMTokenInterface.sol";
+import {Proposal25} from "./Proposal25.sol";
+
+interface YYCRV {
+    function deposit(uint256 _amount) external;
+    function withdraw(uint256 shares) external;
+}
+
+contract Proposal25test is YAMTest {
+    Proposal25 private proposal;
+
+    IERC20 internal constant WETH =
+        IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IERC20 internal constant YAM =
+        IERC20(0x0AaCfbeC6a24756c20D41914F2caba817C0d8521);
+    IERC20 internal constant UMA =
+        IERC20(0x04Fa0d235C4abf4BcF4787aF4CF447DE572eF828);
+    IERC20 internal constant SUSHI =
+        IERC20(0x6B3595068778DD592e39A122f4f5a5cF09C90fE2);
+    address internal constant RESERVES =
+        0x97990B693835da58A281636296D2Bf02787DEa17;
+    address internal constant MULTISIG =
+        0x744D16d200175d20E6D8e5f405AEfB4EB7A962d1;
+    address internal constant INCENTIVIZER =
+        0xD67c05523D8ec1c60760Fd017Ef006b9F6e496D0;
+    IERC20 internal constant SCJAN6 =
+        IERC20(0x0B4470515228625ef51E6DB32699989046fCE4De);
+    IERC20 internal constant SCDEC2 =
+        IERC20(0xf447EF251De50E449107C8D687d10C91e0b7e4D4);
+    IERC20 internal constant SCNOV3 =
+        IERC20(0xff8f62855fD433347BbE62f1292F905f7aC1DF9d);
+    IUSynthFarming internal UGASFarming =
+        IUSynthFarming(0xDFE435ade40Bbf7476a0847d5ae6B1df9BD5Ba30);
+    IUSynthFarming internal USTONKSFarming =
+        IUSynthFarming(0x9789204c43bbc03E9176F2114805B68D0320B31d);
+    IUSynthFarming internal UPUNKSFarming =
+        IUSynthFarming(0xA5163cE5331413715c1023f28499cbdC9a8b8b3E);
+
+    function setUp() public {
+        setUpYAMTest();
+        proposal = new Proposal25();
+    }
+
+    function test_proposal_25() public {
+        address[] memory targets = new address[](7);
+        uint256[] memory values = new uint256[](7);
+        string[] memory signatures = new string[](7);
+        bytes[] memory calldatas = new bytes[](7);
+        string
+            memory description = "Contributors comps for April, exit UGAS USTONKS UPUNKS positions, claiming all reward tokens from synths farms, transfer yam from vesting pool to reserves, transfer yam tokens to mainnet multisig, mint and send yam to reserves and claim rewards for reserves.";
+
+        // Set proposal as sub gov for vestingPool
+        targets[0] = address(vestingPool);
+        signatures[0] = "setSubGov(address,bool)";
+        calldatas[0] = abi.encode(address(proposal), true);
+
+        targets[1] = address(INCENTIVIZER);
+        signatures[1] = "sushiToReserves(uint256)";
+        calldatas[1] = abi.encode(type(uint256).max);
+
+        // Whitelist proposal to withdraw usdc
+        targets[2] = address(reserves);
+        signatures[2] = "whitelistWithdrawals(address[],uint256[],address[])";
+        address[] memory whos = new address[](1);
+        uint256[] memory amounts = new uint256[](1);
+        address[] memory tokens = new address[](1);
+
+        whos[0] = address(proposal);
+        amounts[0] = type(uint256).max;
+        tokens[0] = address(USDC);
+
+        calldatas[2] = abi.encode(whos, amounts, tokens);
+
+        targets[3] = address(UGASFarming);
+        signatures[3] = "setIsSubGov(address,bool)";
+        calldatas[3] = abi.encode(address(proposal), true);
+
+        targets[4] = address(USTONKSFarming);
+        signatures[4] = "setIsSubGov(address,bool)";
+        calldatas[4] = abi.encode(address(proposal), true);
+
+        targets[5] = address(UPUNKSFarming);
+        signatures[5] = "setIsSubGov(address,bool)";
+        calldatas[5] = abi.encode(address(proposal), true);
+
+        targets[6] = address(yamV3);
+        signatures[6] = "mint(address,uint256)";
+        calldatas[6] = abi.encode(address(proposal), 370000 * (10**18));
+
+        // Get quorum for test proposal
+        getQuorum(yamV3, proposer);
+        bing();
+
+        // Post, vote and execute proposal
+        rollProposal(targets, values, signatures, calldatas, description);
+        bing();
+
+        proposal.execute();
+        ff(720 minutes);
+
+        UGASFarming.update_twap();
+        UPUNKSFarming.update_twap();
+        ff(61 minutes);
+
+        UGASFarming.exit();
+        UPUNKSFarming.exit();
+
+        // Reserves should have the weth we should have
+        assertTrue(
+            IERC20(WETH).balanceOf(address(reserves)) > 540000000000000000000
+        );
+
+        // Reserves should have the yam we should have
+        assertTrue(
+            IERC20(YAM).balanceOf(address(reserves)) > 280000000000000000000000
+        );
+
+        // Reserves should have the uma we should have
+        assertEq(
+            IERC20(UMA).balanceOf(address(reserves)),
+            30860162972362817448015
+        );
+
+        // Reserves should have the sushi rewards
+        assertTrue(
+            IERC20(SUSHI).balanceOf(address(reserves)) > 4600000000000000000000
+        );
+
+        // Reserves should have the yUSDC we should have
+        assertTrue(IERC20(yUSDC).balanceOf(address(reserves)) > 1560000000000);
+
+        // Reserves should have the USDC we should have
+        assertTrue(IERC20(USDC).balanceOf(address(reserves)) > 67000000000);
+
+        // Reserves should have the synths success tokens we should have
+        assertEq(
+            IERC20(SCJAN6).balanceOf(address(reserves)),
+            4092365051836079280568
+        );
+        assertEq(
+            IERC20(SCDEC2).balanceOf(address(reserves)),
+            4006124534396574916305
+        );
+        assertEq(
+            IERC20(SCNOV3).balanceOf(address(reserves)),
+            1796702909564921172554
+        );
+
+        // No USDC or yUSDC should be left in the proposal
+        assertEq(IERC20(USDC).balanceOf(address(proposal)), 0);
+        assertEq(IERC20(yUSDCV2).balanceOf(address(proposal)), 0);
+        assertEq(IERC20(yUSDC).balanceOf(address(proposal)), 0);
+    }
+
+}
+
+interface IUSynthFarming {
+    function _approveEnter() external;
+    function _approveExit() external;
+    function _getTokenFromHere(address token) external;
+    function update_twap() external;
+    function enter() external;
+    function exit() external;
+}
