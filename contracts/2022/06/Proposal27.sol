0d3f1f91f19f68355d8e1a484e750fa51c112a91
rossgalloway <58150151+rossgalloway@users.noreply.github.com>
2022-07-08 13:04:53 -0400

    Update Proposal27.sol
    remove yUSDC address as it isn't used.

--- contracts/2022/06/Proposal27.sol
+++ contracts/2022/06/Proposal27.sol
@@ -10,8 +10,6 @@ contract Proposal27 {
         IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     IERC20 internal constant YAM =
         IERC20(0x0AaCfbeC6a24756c20D41914F2caba817C0d8521);
-    IYVault internal constant yUSDC =
-        IYVault(0xa354F35829Ae975e850e23e9615b11Da1B3dC4DE);
     IERC20 internal constant USDC =
         IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     address internal constant RESERVES =
